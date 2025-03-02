add93c539d492a7970d7e73029a8593fe0fcf0b4
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-03 07:47:06 +0800

    Create 25-get-next-age-2.js

---
+++ 25-get-next-age-2.js
@@ -0,0 +1,17 @@
+/* Get next age II 
+  Complete the function getNextAge such that it returns the age next year (by adding 1 to the current age).
+  Note that the age is provided by the user in a text box (which you can try in the browser tab).
+  However, when the text box is empty, the function returns NaN. We need to fix that and make it show 0 instead of NaN. */
+
+/**
+ * @param {string} age */
+export function getNextAge(age) {
+    if(age===""){
+        return 0;
+    }
+    return Number.parseInt(age,10)+1;
+}
+
+/** passed test cases
+  * Implements getNextAge()
+  * Handles empty text box */
