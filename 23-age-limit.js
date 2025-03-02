c3db874b20a48136d41ddaf48c1a94903dd07a98
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-03 07:22:43 +0800

    Create 23-age-limit.js

---
+++ 23-age-limit.js
@@ -0,0 +1,8 @@
+/* Age limit 
+  Define a variable ageLimit that cannot be re-assigned and give it a value of 18. */
+
+//define a variable "ageLimit" with value 18 that cannot be re-assigned
+const ageLimit=18;
+
+/** passed test cases
+  * define variable */
