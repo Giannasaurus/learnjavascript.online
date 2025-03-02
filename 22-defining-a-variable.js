1cd2de1f5ac4d15e0fbfd6506421ebc01286c2f8
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-03 07:21:35 +0800

    Create 22-defining-a-variable.js

---
+++ 22-defining-a-variable.js
@@ -0,0 +1,11 @@
+/* Define a variable 
+  Define a variable called count with an original value of 0 and then increment it (add 1 to it) on the following line. */
+
+//define a variable "count" with value 0
+    let count=0
+    count+=1;
+//then increment it
+
+/** passed test cases
+  * define variable
+  * increments the value of count by 1 */
