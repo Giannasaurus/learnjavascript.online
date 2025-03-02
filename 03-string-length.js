a6c2f79bd27085ef82c3092de3a84ab862b5bff1
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-02 13:26:26 +0800

    Create 03-string-length.js

---
+++ 03-string-length.js
@@ -0,0 +1,18 @@
+/* String length 
+  Complete the function getCharCount such that it returns the number of characters in the str parameter that it receives.
+  Consider Learn Programming if you're stuck. */
+
+/**
+   * @param {string} str */
+function getCharCount(str) {
+    return str.length;
+}
+
+// Sample usage - do not modify
+console.log(getCharCount("Sam")); // 3
+console.log(getCharCount("Alex 123")); // 8
+console.log(getCharCount("Charley is here")); // 15
+
+/** passed test cases
+  * returns a number
+  * returns the correct length */
