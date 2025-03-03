4471432c546b02c9bf6d29a6257373b582c2785e
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-03 08:28:34 +0800

    edited video preview link

--- 26-text-ellipsis-project-3.js
+++ 26-text-ellipsis-project-3.js
@@ -2,7 +2,7 @@
   Complete the function getDescription such that it returns the first 10 characters of the text parameter it receives followed by an ellipsis. An ellipsis is the dot character written 3 times: ...
   However, whenever the text is 10 characters or less, the ellipsis should not be added because the text is not being trimmed!
   There are multiple ways to solve this challenge. Take your time and make as many mistakes as possible ;) And make sure to try it in the browser tab.
-  video preview: https://imgur.com/gallery/text-ellipsis-project-3-vYp8qFI */
+  video preview: https://imgur.com/a/text-ellipsis-project-3-vYp8qFI */
 
 /**
  * @param {string} text */
