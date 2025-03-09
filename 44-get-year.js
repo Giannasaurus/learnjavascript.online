f0fe97c2279d9dbc088b2244ac171f63f29033f5
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-09 13:06:48 +0800

    Create 44-get-year.js

---
+++ 44-get-year.js
@@ -0,0 +1,19 @@
+/* Get year 
+  Complete the function getYear such that it returns the searchYear (passed as 2nd parameter) when it's found in the array. Otherwise, it should return undefined.
+  Take a look at the sample usage and expected output to better understand how the function should work. */
+
+/**
+ * @param {number[]} years
+ * @param {number} searchYear */
+function getYear(years, searchYear) {
+    return years.find(function(year){
+        return year===searchYear;
+    })
+}
+
+// Sample usage - do not modify
+console.log(getYear([2019, 2020, 2021], 2020)); // 2020
+console.log(getYear([2019, 2020, 2021], 1990)); // undefined
+
+/** passed test cases
+  * Implements getYear */
